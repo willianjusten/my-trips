1856bb598edbe5ffc86bfc0d02ff4b672d97b619
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-02-27 19:22:13 -0300

    Rename stuff to my-trips

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project to show my favorite spots in the world."
         />
       </Head>
       <GlobalStyles />
