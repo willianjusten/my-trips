53499dcffa97f0ccf2f00c95e821750cac46c9a1
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-02-28 15:33:29 -0300

    Add MapBox styles and env variables

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -15,6 +15,24 @@ export type MapProps = {
   places?: Place[]
 }
 
+const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
+const MAPBOX_USERID = process.env.NEXT_PUBLIC_MAPBOX_USERID
+const MAPBOX_STYLEID = process.env.NEXT_PUBLIC_MAPBOX_STYLEID
+
+const CustomTileLayer = () => {
+  return MAPBOX_API_KEY ? (
+    <TileLayer
+      attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+    />
+  ) : (
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  )
+}
+
 const Map = ({ places }: MapProps) => {
   const router = useRouter()
 
@@ -24,10 +42,7 @@ const Map = ({ places }: MapProps) => {
       zoom={3}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <CustomTileLayer />
 
       {places?.map(({ id, slug, name, location }) => {
         const { latitude, longitude } = location
