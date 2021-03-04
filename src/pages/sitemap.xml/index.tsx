5ddf4fbe27e37487b5a335d20879c1cf8cd6caa2
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-03-04 12:55:04 -0300

    Add sitemap generation

---
+++ src/pages/sitemap.xml/index.tsx
@@ -0,0 +1,31 @@
+import { getServerSideSitemap } from 'next-sitemap'
+import { GetServerSideProps } from 'next'
+import client from 'graphql/client'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
+import { GET_PLACES } from 'graphql/queries'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
+
+  const fields = places.map(({ slug }) => ({
+    loc: `https://my-trips.willianjusten.com.br/${slug}`,
+    lastmod: new Date().toISOString()
+  }))
+
+  fields.push(
+    {
+      loc: 'https://my-trips.willianjusten.com.br',
+      lastmod: new Date().toISOString()
+    },
+    {
+      loc: 'https://my-trips.willianjusten.com.br/about',
+      lastmod: new Date().toISOString()
+    }
+  )
+
+  return getServerSideSitemap(ctx, fields)
+}
+
+// Default export to prevent next.js errors
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default () => {}
