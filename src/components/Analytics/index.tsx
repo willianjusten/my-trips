b91c01c7cf1534935d92bfd39edd12983c6cb6fc
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-06-30 19:16:42 -0300

    Update Google Analytics

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -1,24 +1,22 @@
+import Script from 'next/script'
 import React from 'react'
 
-const Analytics = () => (
-  <>
-    <script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
-    />
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}', {
-              page_path: window.location.pathname,
-            });
-          `
-      }}
-    />
-  </>
-)
+function Analytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}');
+        `}
+      </Script>
+    </>
+  )
+}
 
 export default Analytics
